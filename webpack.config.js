f89f9174fe19d6e4f2a35c07d7453117b5bb5ea9
Wilfried-Tech <wilfriedlarry63@gmail.com>
2022-04-05 23:55:00 +0100

    Committed on TermuxApp
    direct

--- webpack.config.js
+++ webpack.config.js
@@ -46,8 +46,8 @@ const config = {
               test: /\.js$/,
               use: 'babel-loader',
               exclude: /node_modules/
-              }
-      {
+              },
+        {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
